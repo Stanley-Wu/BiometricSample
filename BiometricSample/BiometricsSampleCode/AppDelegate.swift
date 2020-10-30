af12f2cf2ddc74661486b2d3911cd7d15d23f417
Stanley <stanleypipi@hotmail.com>
2020-10-30 15:52:59 +0800

    update AppDelegate and SceneDelegate

--- BiometricSample/BiometricsSampleCode/AppDelegate.swift
+++ BiometricSample/BiometricsSampleCode/AppDelegate.swift
@@ -2,8 +2,8 @@
 //  AppDelegate.swift
 //  BiometricSample
 //
-//  Created by Dlink on 2020/10/29.
-//  Copyright © 2020 Dlink. All rights reserved.
+//  Created by Stanley on 2020/10/29.
+//  Copyright © 2020 Stanley. All rights reserved.
 //
 
 import UIKit
